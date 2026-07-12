bf000f87ae15172f5c2a23cfb7b0cf99eb1dfe89
Dilip17-cyber <lamjung0088@gmail.com>
2026-07-12 22:39:09 +0545

    feat(income):get income

--- src/modules/income/income.router.ts
+++ src/modules/income/income.router.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { authGuard } from '../../middleware/authGuard.js'
-import { createIncome } from './income.controller.js'
+import { createIncome, getIncome } from './income.controller.js'
 
 const router=express.Router()
 
 router.post("/",authGuard,createIncome)
+router.get("/",authGuard,getIncome)
 
 export default router
