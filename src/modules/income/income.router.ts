3d19dbf8b493549af14997b4b0751aa8b6c05227
Dilip17-cyber <lamjung0088@gmail.com>
2026-07-13 22:52:25 +0545

    feat(income):Added getIncome , getIncome by id and update

--- src/modules/income/income.router.ts
+++ src/modules/income/income.router.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import { authGuard } from '../../middleware/authGuard.js'
-import { createIncome, getIncome } from './income.controller.js'
+import { createIncome, getIncome, getIncomeById } from './income.controller.js'
 
 const router=express.Router()
 
 router.post("/",authGuard,createIncome)
 router.get("/",authGuard,getIncome)
-
+router.get("/:id",authGuard,getIncomeById)
 export default router
