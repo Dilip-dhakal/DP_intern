73c335427007a92541a59a99f53e5b15cedc95a4
Dilip17-cyber <lamjung0088@gmail.com>
2026-07-14 20:44:26 +0545

    feat(rate):added rate limiter

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet'
 import authRoutes from "./modules/auth/auth.routes.js"
 import incomeRoutes from "./modules/income/income.router.js"
 import morgan from 'morgan'
+import rateLimit from 'express-rate-limit'
 const app=express()
 
 
@@ -11,9 +12,15 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(helmet()); 
 app.use(morgan("dev"))
+app.use(rateLimit)
 
 
 app.use("/api/auth",authRoutes)
 app.use("/api/income",incomeRoutes)
 
+
+authRoutes.use(rateLimit({
+    windowMs:60*60*1000,
+    max:5
+}))
 export default app
